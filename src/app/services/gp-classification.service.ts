295031d9dc698844d6cbf27a049c9200a10fbf6e
HammerHead300 <pablo.olalla@gmail.com>
2025-06-09 09:52:33 +0200

    arreglado el problema de las CORS

--- src/app/services/gp-classification.service.ts
+++ src/app/services/gp-classification.service.ts
@@ -9,7 +9,7 @@ export class GpClassificationService {
   constructor(private http:HttpClient) { }
 
   classificationList(){
-    const url = "https://api.motogp.pulselive.com/motogp/v1/results/standings?seasonUuid=ae6c6f0d-c652-44f8-94aa-420fc5b3dab4&categoryUuid=e8c110ad-64aa-4e8e-8a86-f2f152f6a942";
+    const url = "api/results/standings?seasonUuid=ae6c6f0d-c652-44f8-94aa-420fc5b3dab4&categoryUuid=e8c110ad-64aa-4e8e-8a86-f2f152f6a942";
     return this.http.get(url);
   }
 }
