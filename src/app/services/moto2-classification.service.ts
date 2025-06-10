6be7ceda8bd5181958508e8f8d1d4412dec3f366
hammerSecond <holacuentaburner@gmail.com>
2025-06-10 17:59:18 +0200

    nah no funciona

--- src/app/services/moto2-classification.service.ts
+++ src/app/services/moto2-classification.service.ts
@@ -10,7 +10,7 @@ export class Moto2ClassificationService {
 
 
   classificationList(){
-    const url = "https://api.motogp.pulselive.com/motogp/v1/results/standings?seasonUuid=ae6c6f0d-c652-44f8-94aa-420fc5b3dab4&categoryUuid=549640b8-fd9c-4245-acfd-60e4bc38b25c";
+    const url = "api/results/standings?seasonUuid=ae6c6f0d-c652-44f8-94aa-420fc5b3dab4&categoryUuid=549640b8-fd9c-4245-acfd-60e4bc38b25c";
     return this.http.get(url);
   }
 }
