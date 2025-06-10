6f6ff165951ce42b9682a707bda8b715809a95a7
HammerHead300 <pablo.olalla@gmail.com>
2025-06-10 23:28:38 +0200

    dist del 2º intento

--- dist/moto-data-base/server/angular-app-manifest.mjs
+++ dist/moto-data-base/server/angular-app-manifest.mjs
@@ -20,11 +20,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 719, hash: '1fc7b8b3f65da527d04999f36687e69e246533c1116bd98d518fada817189980', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1005, hash: '20087d1edcd79ef2b6c7537ff88b11295fce11b3f1f91cf781b9f03a0c7b0df2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'moto2/index.html': {size: 6092, hash: '4391f2a091b30bf42643b9bc79524d595d3f211bafa74136697b6dfe549007f9', text: () => import('./assets-chunks/moto2_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 6061, hash: '8d908882fb9f2b27275f0620ac665824a1b745e0209ebfb483cede589d269f3e', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'moto3/index.html': {size: 6099, hash: '136d5fe72056323f3de9fa7e825b06bf70da791ed34d90c60364c639e6adeaad', text: () => import('./assets-chunks/moto3_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 719, hash: '0f8349e6e9b0d4a4d61a28819429bf2acd2e5af37ff753363c3f33896249ddd6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1005, hash: 'bf5281db9c495ab5c2393e9e3152fbbbfc1eaaf04b8150e502dc06ee9d772ba4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 6061, hash: 'c81cef7e6749e4654144fc438e77926b8dce589802afd1d1ed7f7e09bfaf8b1d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'moto2/index.html': {size: 6092, hash: '96150e46f97fe2bf58f91f50a4eda4b40db86f655cfadf003268d80430d905a0', text: () => import('./assets-chunks/moto2_index_html.mjs').then(m => m.default)},
+    'moto3/index.html': {size: 6099, hash: 'bb78e767a0eb49969f81f5065d6728f4dc363f220ada767671c5dfb70a5ba403', text: () => import('./assets-chunks/moto3_index_html.mjs').then(m => m.default)},
     'styles-HADAENN7.css': {size: 102, hash: 'mDZC83XjiM8', text: () => import('./assets-chunks/styles-HADAENN7_css.mjs').then(m => m.default)}
   },
 };
