9941d8dd412b87500512f45d290acd5b012f2844
HammerHead300 <pablo.olalla@gmail.com>
2025-06-11 09:59:28 +0200

    aplicados estilos para vistas de tablet y escritorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MenuComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
